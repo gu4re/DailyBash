f3dbf1b4a4169c85718f837c1afc5f12b41b3b05
ER4UG.reverse <99036688+gu4re@users.noreply.github.com>
2023-03-17 00:45:37 +0100

    debugging

---
+++ _hashchecker/hashchecker.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Function that checks the hash
+function hashchecker {
+
+	file=$1
+	hash_file=$($3 "$file" | awk '{ print $1 }')
+
+	good_hash=$(cat $2 | awk '{ print $1 }')
+
+	if [ "$hash_file" = "$good_hash" ]; then
+		echo "All okay! Hashes okay!"
+	else
+		echo "Boop Boop! Different Hash!"
+	fi
+
+	exit 0
+}
+
+# Check for arguments
+if [ "$#" -ne 3 ]; then
+	echo "Error: Bad arguments"
+	echo "Correct usage: hashchecker [FILE] [HASH FILE] [HASHTYPE]"
+	exit 1
+fi
+
+# Global Variables
+file=$1
+hash_file=$2
+hash_function=$3
+
+if [ "$hash_function" = "sha256" ]; then
+	if ! echo "$hash_file" | grep -qE '\.sha256$'; then
+		echo "Error. Bad hash file. Need ".sha256" file"
+		exit 1
+	else
+		hash_function="sha256sum"
+		hashchecker $file $hash_file $hash_function
+	fi
+
+elif [ "$hash_function" = "md5" ]; then
+	if ! echo "$hash_file" | grep -qE '\.md5$'; then
+		echo "Error. Bad hash file. Need ".md5" file"
+		exit 1
+	else
+		hashchecker $file $hash_file $hash_function
+	fi
+else
+	echo "Error. Bad hash function. Only sha256 or md5 is allowed"
+fi
